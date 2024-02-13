e2b2bf5aa0f2eac4b9483880401881d9fbbe9c96
Ethiopia12345 <101978426+mijaleta@users.noreply.github.com>
2024-02-13 21:12:24 +0300

    Create Declare a Read-Only Variable with the const Keyword.js

---
+++ Declare a Read-Only Variable with the const Keyword.js
@@ -0,0 +1,6 @@
+// The keyword let is not the only new way to declare variables. In ES6, you can also declare variables using the const keyword.
+
+// const has all the awesome features that let has, with the added bonus that variables declared using const are read-only. They are a constant value, which means that once a variable is assigned with const, it cannot be reassigned:
+
+// const FAV_PET = "Cats";
+// FAV_PET = "Dogs";
